2f75e7d0b7cb54738c07c9e849e15cd224d811f1
Jens Opsvik <jensemann92@gmail.com>
2019-10-22 16:12:55 +0200

    commit

--- src/main/java/no/jeppe/jbdc/ParticipantDao.java
+++ src/main/java/no/jeppe/jbdc/ParticipantDao.java
@@ -1,18 +1,48 @@
 package no.jeppe.jbdc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ParticipantDao {
 
-    private List<String> participants = new ArrayList<>();
+    private DataSource dataSource;
 
-    public void insertParticipant(String participantName) {
-        participants.add(participantName);
+    public ParticipantDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public List<String> listAll() {
-        return participants;
+    public void insertParticipant(String participantName) {
+
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(
+                    "insert into participants (name) values (?)");
+            statement.setString(1, participantName);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> listAll() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from participants"
+            )) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    List<String> result = new ArrayList<>();
+
+                    while (resultSet.next()) {
+                        result.add(resultSet.getString("name"));
+                    }
+
+                    return result;
+                }
+            }
+        }
     }
 }
--- src/test/java/no/jeppe/jbdc/ProjectTest.java
+++ src/test/java/no/jeppe/jbdc/ProjectTest.java
@@ -1,7 +1,10 @@
 package no.jeppe.jbdc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,8 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProjectTest {
 
     @Test
-    void shouldRetriveProjectParticipant() {
-        ParticipantDao dao = new ParticipantDao();
+    void shouldRetrieveProjectParticipant() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table participants (name varchar(100))"
+        );
+
+        ParticipantDao dao = new ParticipantDao(dataSource);
         String participantName = pickOne(new String[] {"Knut", "Asbjørn", "Magda", "Silje"});
         dao.insertParticipant(participantName);
         assertThat(dao.listAll()).contains(participantName);
